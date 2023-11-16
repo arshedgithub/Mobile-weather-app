ba73004b793aac7ca5ddab663ef1b03ef78680fa
arshedgithub <arshedahmedmail@gmail.com>
2023-11-16 07:54:29 +0530

    make the card blur

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -37,27 +39,36 @@ class _WeatherScreenState extends State<WeatherScreen> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16)
                 ),
-                child: const Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        '300 F',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 34,
-                        ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 10,
+                      sigmaY: 10
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300 F',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 34,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,  
+                          ),
+                          Text(
+                            'Rain', 
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
                       ),
-                      const SizedBox(height: 16),
-                      Icon(
-                        Icons.cloud,
-                        size: 64,  
-                      ),
-                      Text(
-                        'Rain', 
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
+                    ),
                   ),
                 ),
               ),
