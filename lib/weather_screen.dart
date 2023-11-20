51c6f893dd6224b6ec361c9921034cf602a953e8
arshedgithub <arshedahmedmail@gmail.com>
2023-11-20 11:18:19 +0530

    add reuseable card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -73,140 +73,56 @@ class _WeatherScreenState extends State<WeatherScreen> {
             ),
           ),
           const SizedBox(height: 20),
-          SingleChildScrollView(
+          const SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(children: [
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  padding: const EdgeInsets.all(8.0),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '03.00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '320.18',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  padding: const EdgeInsets.all(8.0),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '03.00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '320.18',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  padding: const EdgeInsets.all(8.0),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '03.00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '320.18',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  padding: const EdgeInsets.all(8.0),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '03.00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '320.18',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              HourlyForecastItem(),
+              HourlyForecastItem(),
+              HourlyForecastItem(),
+              HourlyForecastItem(),
+              HourlyForecastItem(),
             ]),
           )
         ]),
+      ),
+    );
+  }
+}
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+        child: const Column(
+          children: [
+            Text(
+              '03.00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Icon(
+              Icons.cloud,
+              size: 32,
+            ),
+            SizedBox(height: 8),
+            Text(
+              '320.18',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
