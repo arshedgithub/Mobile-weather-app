4ab613b31acccb6de9d321ebd6412a46a31a4ab8
arshedgithub <arshedahmedmail@gmail.com>
2023-11-20 11:12:53 +0530

    make scrollable

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -73,136 +73,139 @@ class _WeatherScreenState extends State<WeatherScreen> {
             ),
           ),
           const SizedBox(height: 20),
-          Row(children: [
-            Card(
-              elevation: 6,
-              child: Container(
-                width: 100,
-                padding: const EdgeInsets.all(8.0),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                child: const Column(
-                  children: [
-                    Text(
-                      '03.00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(children: [
+              Card(
+                elevation: 6,
+                child: Container(
+                  width: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  child: const Column(
+                    children: [
+                      Text(
+                        '03.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Icon(
-                      Icons.cloud,
-                      size: 32,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      '320.18',
-                      style: TextStyle(
-                        fontSize: 16,
+                      SizedBox(height: 8),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8),
+                      Text(
+                        '320.18',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Card(
-              elevation: 6,
-              child: Container(
-                width: 100,
-                padding: const EdgeInsets.all(8.0),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                child: const Column(
-                  children: [
-                    Text(
-                      '03.00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+              Card(
+                elevation: 6,
+                child: Container(
+                  width: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  child: const Column(
+                    children: [
+                      Text(
+                        '03.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Icon(
-                      Icons.cloud,
-                      size: 32,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      '320.18',
-                      style: TextStyle(
-                        fontSize: 16,
+                      SizedBox(height: 8),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8),
+                      Text(
+                        '320.18',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Card(
-              elevation: 6,
-              child: Container(
-                width: 100,
-                padding: const EdgeInsets.all(8.0),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                child: const Column(
-                  children: [
-                    Text(
-                      '03.00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+              Card(
+                elevation: 6,
+                child: Container(
+                  width: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  child: const Column(
+                    children: [
+                      Text(
+                        '03.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Icon(
-                      Icons.cloud,
-                      size: 32,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      '320.18',
-                      style: TextStyle(
-                        fontSize: 16,
+                      SizedBox(height: 8),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8),
+                      Text(
+                        '320.18',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Card(
-              elevation: 6,
-              child: Container(
-                width: 100,
-                padding: const EdgeInsets.all(8.0),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(16)),
-                child: const Column(
-                  children: [
-                    Text(
-                      '03.00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+              Card(
+                elevation: 6,
+                child: Container(
+                  width: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  child: const Column(
+                    children: [
+                      Text(
+                        '03.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Icon(
-                      Icons.cloud,
-                      size: 32,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      '320.18',
-                      style: TextStyle(
-                        fontSize: 16,
+                      SizedBox(height: 8),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8),
+                      Text(
+                        '320.18',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ])
+            ]),
+          )
         ]),
       ),
     );
