79fc2851d62691811d06421d40e4566e0b313292
arshedgithub <arshedahmedmail@gmail.com>
2023-11-27 20:50:06 +0530

    call API

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/additional_info.dart';
 import 'package:weather_app/hourly_forecast_item.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/secrets.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
+
+  Future getCurrentWeather() async {
+    String cityName = 'London';
+    http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$openweatherAPIkey'));
+  }
 
   @override
   Widget build(BuildContext context) {
